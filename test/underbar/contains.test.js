b71d534610923bcbbbd22559fe858ae9caad4e13
mttrys <mttrys@gmail.com>
2017-05-10 10:26:06 -0700

    Tested contains

--- test/underbar/contains.test.js
+++ test/underbar/contains.test.js
@@ -2,19 +2,23 @@ const _ = require('../../underbar');
 
 describe('contains()', () => {
   it('returns true if an array contains the target', () => {
-    // Your code goes here
+    let ar = ['a','b','c','d','e'];
+    expect(_.contains(ar, 'a')).toBe(true);
   });
 
   it('returns false if an array does not contain the target', () => {
-    // Your code goes here
+    let ar = ['a','b','c','d','e'];
+    expect(_.contains(ar, 'f')).toBe(false);
   });
 
   it('returns true if the target value is among the values of an object', () => {
-    // Your code goes here
+    let ob = {0: 'a', 1: 'b'};
+    expect(_.contains(ob, 'a')).toBe(true);
   });
 
   it('returns false if the target value is not among the values of an object', () => {
-    // Your code goes here
+    let ob = {0: 'a', 1: 'b'};
+    expect(_.contains(ob, 'c')).toBe(false);
   });
 
 });
