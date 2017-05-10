029d29f3e8ab21e7cd24c9b7e8c5d0764847e216
mttrys <mttrys@gmail.com>
2017-05-10 10:03:20 -0700

    Tested identity

--- test/underbar/identity.test.js
+++ test/underbar/identity.test.js
@@ -2,18 +2,18 @@ const _ = require('../../underbar');
 
 describe('identity()', () => {
   it('returns null if given null', () => {
-    // Your code goes here
+    expect(_.identity(null)).toBe(null)
   });
 
   it('returns the number if given a number', () => {
-    // Your code goes here
+    expect(_.identity(1)).toBe(1)
   });
 
   it('returns the same array if given an array', () => {
-    // Your code goes here
+    expect(_.identity([0,1,2])).toEqual([0,1,2])
   });
 
   it('returns the same object if given an object', () => {
-    // Your code goes here
+    expect(_.identity({0: 'a', 1: 'b'})).toEqual({0: 'a', 1: 'b'})
   });
 });
